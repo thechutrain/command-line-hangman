54a6e88d81e9e768b0f8baad586e275785f7144d
thechutrain <chu.chuyalan@gmail.com>
2017-02-06 14:32:11 -0500

    getRandomQuote is now a promise :)

--- quoteAPI.js
+++ quoteAPI.js
@@ -1,13 +1,36 @@
 var unirest = require("unirest");
 var private = require("./private");
 
-unirest.post("https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous")
-    .header("X-Mashape-Key", private.quote_API_KEY)
-    .header("Content-Type", "application/x-www-form-urlencoded")
-    .header("Accept", "application/json")
-    .end(function (result) {
-    // console.log(result.status, result.headers, result.body);
-    console.log(result.body);
-});
+function getRandomQuote(){
+    return new Promise(function(resolve, reject){
+        unirest.post("https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous")
+        .header("X-Mashape-Key", private.quote_API_KEY)
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .header("Accept", "application/json")
+        .end(function (result) {
+        // console.log(result.status, result.headers, result.body);
+        // console.log("------------");
+        // console.log(result.status);
+        if (result.status !== 200) reject({"status_code": result.status});
+        else {
+        // console.log(result.body);
+        var body = JSON.parse(result.body);
+        var quoteObj = { quote: body.quote, author: body.author };
+        // console.log(quoteObj);
+        resolve(quoteObj);
+        }
+    
+        }) // closes .end()
+    }) // closes promise
+}
 
+
+// TESTING!
 // console.log(private.quote_API_KEY);
+getRandomQuote().then(
+    function(response){
+    console.log("---- from a fulfilled promise -----");
+    console.log(response);
+    }, function(err){
+        console.log(err);
+    })
